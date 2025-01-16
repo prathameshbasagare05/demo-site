8830365d61d7ce5f64b0219c79971aa86a935475
Prathamesh Basagare <prathamesh.basagare@wisdmlabs.com>
2025-01-16 19:07:52 +0530

    Changes in pricing table

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const navLinks = document.querySelectorAll(".nav-link");
 let nav = document.querySelector(".nav-link")
 navLinks.forEach(link => {
     link.addEventListener("click", () => {
-        // navLinks.forEach(nav => nav.classList.remove("active"));
         nav.classList.remove("active");
         link.classList.add("active");
         nav = link;
